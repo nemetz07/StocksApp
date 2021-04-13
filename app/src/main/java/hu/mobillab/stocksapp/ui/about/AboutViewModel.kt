eb3765e6398e4943a3aa51e2d84075230e706616
Nemetz Norbert <nemetz07@gmail.com>
2021-04-13 14:37:51 +0200

    Created basic fragments/viewmodels/layouts

---
+++ app/src/main/java/hu/mobillab/stocksapp/ui/about/AboutViewModel.kt
@@ -0,0 +1,7 @@
+package hu.mobillab.stocksapp.ui.about
+
+import androidx.lifecycle.ViewModel
+
+class AboutViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/hu/mobillab/stocksapp/ui/stocks/StocksViewModel.kt
@@ -0,0 +1,7 @@
+package hu.mobillab.stocksapp.ui.stocks
+
+import androidx.lifecycle.ViewModel
+
+class StocksViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
