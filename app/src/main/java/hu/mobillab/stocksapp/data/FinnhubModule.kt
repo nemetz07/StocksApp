04a1c7266d295f6128f330dfd683155d2325e3c4
Nemetz Norbert <nemetz07@gmail.com>
2021-04-24 12:25:35 +0200

    Added webservice provider

---
+++ app/src/main/java/hu/mobillab/stocksapp/data/FinnhubModule.kt
@@ -0,0 +1,37 @@
+package hu.mobillab.stocksapp.data
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.mobillab.stocksapp.tools.GeneratedCodeConverters
+import hu.mobillab.stocksapp.util.FINNHUB_TOKEN
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FinnhubModule {
+
+    @Provides
+    fun provideFinnhubService(): FinnhubApi {
+        return Retrofit.Builder()
+            .baseUrl("https://finnhub.io/api/v1/")
+            .client(
+                OkHttpClient.Builder().also {
+                    it.addInterceptor { chain ->
+                        val request =
+                            chain.request()
+                                .newBuilder()
+                                .addHeader("X-Finnhub-Token", FINNHUB_TOKEN)
+                                .build()
+                        chain.proceed(request)
+                    }
+                }.build()
+            )
+            .addConverterFactory(GeneratedCodeConverters.converterFactory())
+            .build()
+            .create(FinnhubApi::class.java)
+    }
+
+}
