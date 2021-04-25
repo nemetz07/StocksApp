a2b8c979e5cbd45d2cbc5e8952e73438defb1c54
Nemetz Norbert <nemetz07@gmail.com>
2021-04-25 10:53:57 +0200

    Added UI elements for testing

--- app/src/main/java/hu/mobillab/stocksapp/ui/detail/DetailViewModel.kt
+++ app/src/main/java/hu/mobillab/stocksapp/ui/detail/DetailViewModel.kt
@@ -2,12 +2,12 @@ package hu.mobillab.stocksapp.ui.detail
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import hu.mobillab.stocksapp.data.StocksRepository
+import hu.mobillab.stocksapp.data.FinnhubRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    private val stocksRepository: StocksRepository
+    private val finnhubRepository: FinnhubRepository
 ) : ViewModel() {
 
     fun getStockDetails() {
